5f31441d30010eaaa4827211d933af0876f6244c
Arun-24 <111137013+Arun-24@users.noreply.github.com>
2025-06-28 13:18:08 +0530

    Add files via upload

---
+++ Assignment5/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ProductList from './ProductList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>🛍️ Featured Products</h1>
+      <ProductList />
+    </div>
+  );
+}
+
+export default App;
